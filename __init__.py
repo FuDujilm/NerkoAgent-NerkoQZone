0b7c31d64bd59bfe4efaa6425a39861617698cc2
LimeNeko <72288165+FuDujilm@users.noreply.github.com>
2025-11-11 13:17:55 +0800

    fix: fallback sandbox test methods for older sdk

--- __init__.py
+++ __init__.py
@@ -1,4 +1,22 @@
 
-from .plugin import plugin
+"""Package exports for the QZone Nerko plugin."""
 
-__all__ = ["plugin"]
+from .plugin import (
+    check_feeds,
+    plugin,
+    qzone_diag,
+    qzone_live_post,
+    qzone_live_post_llm,
+    test_model_generation,
+    test_napcat_connection,
+)
+
+__all__ = [
+    "plugin",
+    "qzone_diag",
+    "check_feeds",
+    "qzone_live_post",
+    "qzone_live_post_llm",
+    "test_napcat_connection",
+    "test_model_generation",
+]
